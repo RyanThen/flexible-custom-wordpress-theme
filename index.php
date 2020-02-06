90e9d5ff2d6beec553fc0605b1b4bb58691986a2
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-02-06 16:43:05 -0500

    made code more reusable

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
-<?php get_header(); ?>
+<?php
+get_header();
+pageBanner(array(
+	'title' => 'Welcome to our blog!',
+	'subtitle' => 'Keep up with our latest news'
+));
+?>
 
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('udemy-course-resources/images/library-hero.jpg') ?>);"></div>
-    <div class="page-banner__content container t-center c-white">
-      <h1 class="headline headline--large">Welcome!</h1>
-      <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-      <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-      <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
-    </div>
-  </div>
 
   <div class="full-width-split group">
     <div class="full-width-split__one">
