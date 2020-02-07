7d3ea2963f2dfa6e649dd237536a931c12e3b530
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-02-07 18:42:22 -0500

    initial commit of functions file

---
+++ functions.php
@@ -0,0 +1,78 @@
+<?php
+
+function pageBanner($args = NULL) {
+	
+	if (!$args['title']) {
+		$args['title'] = get_the_title();
+	}
+	if (!$args['subtitle']) {
+		$args['subtitle'] = get_field('page_banner_subtitle');
+	}
+	if (!$args['image']) {
+		if (get_field('page_banner_background_image')) {
+			$args['image'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+		} else {
+			$args['image'] = get_theme_file_uri('images/ocean.jpg');
+		}
+	}
+	
+	
+	?>
+	<div class="page-banner">
+		<div class="page-banner__bg-image" style="background-image: url(<?php echo $args['image']; ?>);"></div>
+		<div class="page-banner__content container container--narrow">
+			<h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+			<div class="page-banner__intro">
+				<p><?php echo $args['subtitle']; ?></p>
+			</div>
+		</div>
+	</div>
+<?php
+}
+
+function fictional_university_files() {
+	wp_enqueue_style('font-awesome', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+	wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+	wp_enqueue_style('udemy-styles', get_template_directory_uri() . '/css/u-styles.css');
+	wp_enqueue_style('main-stylesheet', get_stylesheet_uri());
+	
+	wp_enqueue_script('main-javascript', get_theme_file_uri('js/scripts-bundled.js'), NULL, microtime(), true);
+}
+add_action('wp_enqueue_scripts', 'fictional_university_files');
+
+function fu_theme_features(){
+	register_nav_menu('primary', 'Primary Navigation Menu');
+	register_nav_menu('footerOne', 'Footer Navigation 1');
+	register_nav_menu('footerTwo', 'Footer Navigation 2');
+	
+	add_theme_support('title-tag');
+	add_theme_support('post-thumbnails');
+	
+	add_image_size('professorLandscape', 400, 260, true);
+	add_image_size('professorPortrait', 480, 650, true);
+	add_image_size('pageBanner', 1500, 350, true);
+}
+
+add_action('after_setup_theme', 'fu_theme_features');
+
+function university_query_adjustments($query) {
+	if (!is_admin() AND is_post_type_archive() AND $query->is_main_query()) {
+		$today = date('Ymd');
+		$query->set('meta_key', 'events_date');
+		$query->set('orderby', 'meta_value_num');
+		$query->set('order', 'ASC');
+		$query->set('meta_query', array(
+				array(
+					'key' => 'events_date',
+					'compare' => '>=',
+					'value' => $today,
+					'type' => 'numeric'
+				)
+			)
+		);
+	}
+}
+
+add_action('pre_get_posts', 'university_query_adjustments');
+
+?>
