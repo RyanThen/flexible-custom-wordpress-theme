bb320a01d688bd6c81efe905fb26ad26d40719cb
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-02-12 12:16:27 -0500

    Adde scripts and scripts-bundled

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// 3rd party packages from NPM
+import $ from 'jquery';
+import slick from 'slick-carousel';
+
+// Our modules / classes
+import MobileMenu from './modules/MobileMenu';
+import HeroSlider from './modules/HeroSlider';
+import Search from './modules/Search';
+
+// Instantiate a new object using our modules/classes
+var mobileMenu = new MobileMenu();
+var heroSlider = new HeroSlider();
+var search = new Search();
