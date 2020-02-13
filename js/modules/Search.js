aca99ce985f6c73f9ebe662fc3af8d2d3b192bd2
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-02-12 19:57:19 -0500

    Added more live search functionality

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -43,9 +43,15 @@ class Search {
 	}
 
 	getResults() {
-		$.getJSON('http://localhost:3000/fictional-university_udemy/wp-json/wp/v2/posts?search=' + this.searchField.val(), function(posts) {
-			alert(posts[0]['title']['rendered']);
-		})
+		$.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+			this.resultsDiv.html( `
+				<h2 class="search-overlay__section-title">General Information</h2>
+				${ posts.length ? `<ul class="link-list min-list">` : `<h4>No information matches your search term</h4>` }
+				${ posts.map( item => `<li><a href="${item.link}">${item.title.rendered}</a></li>` ).join( '' )}
+				${ posts.length ? `</ul>` : '' }
+			` );
+			this.isSpinnerVisible = false;
+		});
 	}
 
 	keyPressDispatcher(e) {
