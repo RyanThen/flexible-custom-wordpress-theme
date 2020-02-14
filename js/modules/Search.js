126546853fd3889520aa711e2d030927065b5d52
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-02-14 07:00:17 -0500

    Added live search functionality

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 class Search {
 	// 1. Initiate object
 	constructor() {
+		this.addSearchHTML();
 		this.openButton = $(".js-search-trigger");
 		this.closeButton = $(".search-overlay__close");
 		this.searchOverlay = $(".search-overlay");
@@ -33,7 +34,7 @@ class Search {
 						this.resultsDiv.html( '<div class="spinner-loader"></div>' );
 						this.isSpinnerVisible = true;
 					}
-					this.typingTimer = setTimeout( this.getResults.bind( this ), 2000 );
+					this.typingTimer = setTimeout( this.getResults.bind( this ), 600 );
 				} else {
 					this.resultsDiv.html( '' );
 					this.isSpinnerVisible = false;
@@ -57,23 +58,43 @@ class Search {
 	keyPressDispatcher(e) {
 		if(e.keyCode === 83 && !this.isOpen && !$("input, textarea").is(":focus")) {
 			this.openOverlay();
-			this.isOpen = true;
 		}
 		if(e.keyCode === 27 && this.isOpen) {
 			this.closeOverlay();
-			this.isOpen = false;
 		}
 	}
 
 	openOverlay() {
 		this.searchOverlay.addClass('search-overlay--active');
 		$('body').addClass('body-no-scroll');
+		this.searchField.val('');
+		setTimeout(() => this.searchField.focus(), 301);
+		this.isOpen = true;
 	}
 
 	closeOverlay(){
 		this.searchOverlay.removeClass('search-overlay--active');
 		$('body').removeClass('body-no-scroll');
+		this.isOpen = false;
 	}
+
+	addSearchHTML() {
+		$("body").append(`
+			<div class="search-overlay">
+				<div class="search-overlay__top">
+					<div class="container">
+						<i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+						<input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+						<i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="container">
+					<div id="search-overlay__results"></div>
+				</div>
+			</div>
+		`)
+	}
+
 }
 
 export default Search;
