98a8bea66b47bb8efa6a725ebccd0ac26637df39
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-02-15 10:48:41 -0500

    initial commit of professor post type template

---
+++ single-professor.php
@@ -0,0 +1,37 @@
+<?php
+
+get_header();
+
+while(have_posts()) {
+	the_post();
+	pageBanner();
+	?>
+	
+	
+	<div class="container container--narrow page-section">
+		<div class="generic-content">
+			<div class="row group">
+				<div class="one-third">
+					<?php the_post_thumbnail('professorPortrait'); ?>
+				</div>
+				<div class="two-third">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+		<?php
+		$relatedPrograms = get_field('catalog_relationships');
+		
+		if($relatedPrograms) {
+			echo '<hr class="section-break">';
+			echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
+			echo '<ul class="link-list min-list">';
+			foreach ($relatedPrograms as $relatedProgram) { ?>
+				<li><a href="<?php echo get_the_permalink($relatedProgram)?>"><?php echo get_the_title($relatedProgram);?></a></li>
+			<?php }
+			echo '</ul>';
+		} ?>
+	</div>
+<?php }; ?>
+
+<?php get_footer();?>
