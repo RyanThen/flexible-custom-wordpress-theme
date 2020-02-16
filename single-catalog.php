7b5947520212a2aba35b730fc75b59b7a2afcc3a
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-02-16 08:26:33 -0500

    Added catalog/programs custom post type

---
+++ single-catalog.php
@@ -0,0 +1,91 @@
+<?php
+
+get_header();
+
+while(have_posts()) {
+	the_post();
+	pageBanner(array(
+		'title' => get_the_title(),
+		'subtitle' => 'What a great program!'
+	))
+	?>
+	
+	<div class="container container--narrow page-section">
+		<div class="metabox metabox--position-up metabox--with-home-link">
+			<p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('catalog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+		</div>
+		<div class="generic-content">
+			<?php the_content();
+			
+			$relatedProfessors = new WP_Query(array(
+				'post_type' => 'professor',
+				'posts_per_page' => -1,
+				'orderby' => 'title',
+				'order' => 'ASC',
+				'meta_query' => array(
+					array(
+						'key' => 'catalog_relationships',
+						'compare' => 'LIKE',
+						'value' => '"' . get_the_ID() . '"'
+					)
+				)
+			));
+			
+			if ($relatedProfessors->have_posts()) {
+				echo '<hr class="section-break">';
+				echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
+				
+				echo '<ul class="professor-cards">';
+				while($relatedProfessors->have_posts()) {
+					$relatedProfessors->the_post(); ?>
+					<li class="professor-card__list-item">
+						<a class="professor-card" href="<?php the_permalink() ?>">
+							<img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>" alt="">
+							<span class="professor-card__name"><?php the_title(); ?></span>
+						</a>
+					</li>
+				<?php }
+				echo '</ul>';
+			}
+			
+			wp_reset_postdata();
+			
+			$today = date('Ymd');
+			$homepageEvents = new WP_Query(array(
+				'post_type' => 'event',
+				'posts_per_page' => 2,
+				'meta_key' => 'events_date',
+				'orderby' => 'meta_value_num',
+				'order' => 'ASC',
+				'meta_query' => array(
+					array(
+						'key' => 'events_date',
+						'compare' => '>=',
+						'value' => $today,
+						'type' => 'numeric'
+					),
+					array(
+						'key' => 'catalog_relationships',
+						'compare' => 'LIKE',
+						'value' => '"' . get_the_ID() . '"'
+					)
+				)
+			));
+			
+			if ($homepageEvents->have_posts()) {
+				echo '<hr class="section-break">';
+				echo '<h2 class="headline headline--medium"> Upcoming ' . get_the_title() . ' Events</h2>';
+				
+				while($homepageEvents->have_posts()) {
+					$homepageEvents->the_post();
+					get_template_part('template-parts/content-event');
+				}
+			}
+			
+			?>
+			
+		</div>
+	</div>
+<?php }; ?>
+
+<?php get_footer();?>
