ee538343329c602b7a40c501765702562487b3cf
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-02-15 19:33:01 -0500

    single event post type template

---
+++ single-event.php
@@ -0,0 +1,35 @@
+<?php
+
+get_header();
+
+while(have_posts()) {
+	the_post();
+	pageBanner(array(
+		'title' => get_the_title(),
+		'subtitle' => 'Take a look at our event'
+	))
+	?>
+	
+	<div class="container container--narrow page-section">
+		<div class="metabox metabox--position-up metabox--with-home-link">
+			<p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events home</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+		</div>
+		<div class="generic-content">
+			<?php the_content(); ?>
+		</div>
+		<?php
+		$relatedPrograms = get_field('catalog_relationships');
+		
+		if($relatedPrograms) {
+			echo '<hr class="section-break">';
+			echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+			echo '<ul class="link-list min-list">';
+			foreach ($relatedPrograms as $relatedProgram) { ?>
+				<li><a href="<?php echo get_the_permalink($relatedProgram)?>"><?php echo get_the_title($relatedProgram);?></a></li>
+			<?php }
+			echo '</ul>';
+		} ?>
+	</div>
+<?php }; ?>
+
+<?php get_footer();?>
