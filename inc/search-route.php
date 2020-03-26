25306aac3dc932686b010dad6740cce1bc0e08d7
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-03-26 12:29:12 -0500

    added keyword search functionality

--- inc/search-route.php
+++ inc/search-route.php
@@ -6,14 +6,15 @@ add_action('rest_api_init', 'universityRegisterSearch');
 function universityRegisterSearch() {
 	register_rest_route('university/v1', 'search', array(
 		'methods' => WP_REST_SERVER::READABLE,   // 'GET'
-		'callback' => 'universitySearchResults'
+		'callback' => 'universitySearchResults'  // return relevant data
 	));
 }
 
   // build custom array to return to 'callback' in register_rest_route
-function universitySearchResults() {
+function universitySearchResults($data) {
 	$professors = new WP_Query(array(
-		'post_type' => 'professor'
+		'post_type' => 'professor',
+		's' => sanitize_text_field($data['term'])
 	));
 	
 	$professorResults = array();
