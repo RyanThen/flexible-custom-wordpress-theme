f4c598a715ed4f62656c215468d3c125aa587f25
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-03-28 15:16:06 -0500

    incorporated custom API into search front-end

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -59,11 +59,35 @@ class Search {
 						${ results.programs.length ? `<ul class="link-list min-list">` : `<h4>No programs match your search <a href="${universityData.root_url}/catalog">View all programs</a></h4>` }
 							${ results.programs.map( item => `<li><a href="${ item.permalink }">${ item.title }</a></li>` ).join( '' )}
 						${ results.programs.length ? `</ul>` : '' }
+						
 						<h2 class="search-overlay__section-title">Professors</h2>
+						${ results.professors.length ? `<ul class="professor-card">` : `<h4>No professors match your search</h4>`}
+							${ results.professors.map( item => `
+								<li class="professor-card__list-item">
+									<a class="professor-card" href="${item.permalink}">
+										<img class="professor-card__image" src="${item.image}" alt="">
+										<span class="professor-card__name">${item.title}</span>
+									</a>
+								</li>
+							` ).join( '' )}
+						${ results.professors.length ? `</ul>` : '' }
 					</div>
 					
 					<div class="one-third">
 						<h2 class="search-overlay__section-title">Events</h2>
+						${ results.events.length ? '' : `<h4>No events match your search <a href="${universityData.root_url}/events">View all events</a></h4>` }
+						${ results.events.map(item => `
+							<div class="event-summary">
+								<a class="event-summary__date t-center" href="${item.permalink}">
+									<span class="event-summary__month">${item.month}</span>
+									<span class="event-summary__day">${item.day}</span>
+								</a>
+								<div class="event-summary__content">
+									<h5 class="event-summary__title headline headline--tiny"><a href="${item.permalink}">${item.title}</a></h5>
+									<p>${item.description} <a href="${item.permalink}" class="nu gray">Learn more</a></p>
+								</div>
+							</div>
+						` ).join( '' )}
 						<h2 class="search-overlay__section-title">Other</h2>
 					</div>
 				</div>
