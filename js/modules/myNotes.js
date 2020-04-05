135e4ce328b0b98c03752de3648e47e918621f5e
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-04-05 12:32:42 -0500

    continued front-end implementation

--- js/modules/myNotes.js
+++ js/modules/myNotes.js
@@ -12,7 +12,21 @@ class MyNotes {
 
 	//Methods
 	deleteNote() {
-		alert('test message');
+		$.ajax({
+			beforeSend: (xhr) => {
+				xhr.setRequestHeader('X-WP-Nonce', universityData.nonce)
+			},
+			url: universityData.root_url + '/wp-json/wp/v2/note/124',
+			type: 'DELETE',
+			success: (response) => {
+				console.log('Congrats!');
+				console.log(response);
+			},
+			error: (response) => {
+				console.log('Sorry!');
+				console.log(response);
+			}
+		});
 	}
 }
 
