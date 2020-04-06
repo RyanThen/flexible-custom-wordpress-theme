f257534bf5d4e3b6413d17a990556b885a2bc340
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-04-05 19:40:25 -0500

    worked on edit and save buttons for front end

--- js/modules/myNotes.js
+++ js/modules/myNotes.js
@@ -8,17 +8,27 @@ class MyNotes {
 	//Events
 	events() {
 		$(".delete-note").on("click", this.deleteNote);
+		$(".edit-note").on("click", this.editNote);
 	}
 
 	//Methods
-	deleteNote() {
+	editNote(e) {
+		var thisNote = $(e.target).parents("li");
+		thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+		thisNote.find(".update-note").addClass("update-note--visible");
+	}
+
+	deleteNote(e) {
+		var thisNote = $(e.target).parents("li");
+
 		$.ajax({
 			beforeSend: (xhr) => {
 				xhr.setRequestHeader('X-WP-Nonce', universityData.nonce)
 			},
-			url: universityData.root_url + '/wp-json/wp/v2/note/124',
+			url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
 			type: 'DELETE',
 			success: (response) => {
+				thisNote.slideUp();
 				console.log('Congrats!');
 				console.log(response);
 			},
