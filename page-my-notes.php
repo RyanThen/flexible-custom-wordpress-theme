c1c1ec9550d5346223c71d1feae10b8147b48fda
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-05-14 20:56:11 -0500

    added create new note markup

--- page-my-notes.php
+++ page-my-notes.php
@@ -13,6 +13,14 @@ while(have_posts()) {
 	?>
 	
 	<div class="container container--narrow page-section">
+		
+		<div class="create-note">
+			<h2 class="headline headline--medium">Create New Note</h2>
+			<input class="new-note-title" placeholder="Title">
+			<textarea class="new-note-body" placeholder="Your note here..."></textarea>
+			<span class="submit-note">Create Note</span>
+		</div>
+		
 		<ul class="min-list link-list" id="my-notes">
 			<?php
 				$userNotes = new WP_Query(array(
