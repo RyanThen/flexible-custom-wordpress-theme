2b3753590422cfa42c450415a516c4fd7e2727cf
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-05-14 20:57:07 -0500

    began adding create new note functionality

--- js/modules/myNotes.js
+++ js/modules/myNotes.js
@@ -10,6 +10,7 @@ class MyNotes {
 		$(".delete-note").on("click", this.deleteNote);
 		$(".edit-note").on("click", this.editNote.bind(this));
 		$(".update-note").on("click", this.updateNote.bind(this));
+		$(".submit-note").on("click", this.createNote.bind(this));
 	}
 
 	//Methods
@@ -73,6 +74,34 @@ class MyNotes {
 			data: ourUpdatedPost,
 			success: (response) => {
 				this.makeNoteReadOnly(thisNote);
+				console.log('Congrats!');
+				console.log(response);
+			},
+			error: (response) => {
+				console.log('Sorry!');
+				console.log(response);
+			}
+		});
+	}
+
+	createNote(e) {
+		var ourNewPost = {
+			'title': $('.new-note-title').val(),
+			'content': $('.new-note-body').val(),
+			'status': 'publish'
+		}
+
+		$.ajax({
+			beforeSend: (xhr) => {
+				xhr.setRequestHeader('X-WP-Nonce', universityData.nonce)
+			},
+			url: universityData.root_url + '/wp-json/wp/v2/note/',
+			type: 'POST',
+			data: ourNewPost,
+			success: (response) => {
+				$('.new-note-title, .new-note-body').val('');
+				$('<li>Real data goes here</li>').prependTo('#my-notes').hide().slideDown();
+
 				console.log('Congrats!');
 				console.log(response);
 			},
